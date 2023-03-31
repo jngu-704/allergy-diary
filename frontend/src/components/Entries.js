6dfebaa7370f42ab5fdcd37bb30473263ae12aca
jngu-704 <80801878+jngu-704@users.noreply.github.com>
2023-03-31 17:14:40 +1100

    frontend new entry can be added

--- frontend/src/components/Entries.js
+++ frontend/src/components/Entries.js
@@ -1,40 +1,92 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import CloseButton from "react-bootstrap/CloseButton";
-import ListGroup from "react-bootstrap/ListGroup";
 import Container from "react-bootstrap/esm/Container";
+import { v4 as uuidv4 } from "uuid";
+import EntryForm from "./EntryForm";
 
-export default function Entries({ diaryEntries }) {
-  const listEntries = diaryEntries.map((entry) => {
-    const listProducts = entry.products.map((product) => (
-      <tr>
-        <td>{product.time}</td>
-        <td>{product.name}</td>
-        <td>{product.ingredients}</td>
-        <td className="align-items-center">
-          <CloseButton />
-        </td>
-      </tr>
-    ));
-    return (
+const entriesData = [
+  {
+    id: uuidv4(),
+    date: "31-03-2023",
+    time: "6:30PM",
+    name: "Burger",
+    ingredients:
+      "Bun, Beef Patty, American Cheese, Ketchup, Pickle Slices, Onions, Mustard",
+  },
+  {
+    id: uuidv4(),
+    date: "31-03-2023",
+    time: "12:30PM",
+    name: "Coca-cola",
+    ingredients:
+      "Carbonated Water, Sugar, Colour (Caramel 150d), Food Acid (338), Flavour, Caffeine",
+  },
+
+  {
+    id: uuidv4(),
+    date: "30-03-2023",
+    time: "6:30PM",
+    name: "Burger",
+    ingredients:
+      "Bun, Beef Patty, American Cheese, Ketchup, Pickle Slices, Onions, Mustard",
+  },
+  {
+    id: uuidv4(),
+    date: "30-03-2023",
+    time: "12:30PM",
+    name: "Coca-cola",
+    ingredients:
+      "Carbonated Water, Sugar, Colour (Caramel 150d), Food Acid (338), Flavour, Caffeine",
+  },
+];
+
+export default function Entries() {
+  const [entries, setEntries] = useState(entriesData);
+
+  // useEffect(() => {}, [entries]);
+
+  const addEntry = (entry) => {
+    setEntries([...entries, entry]);
+    console.log(entries);
+  };
+
+  // const deleteEntry = (id) => {
+  //   const newEntries = [...entries];
+  //   setEntries(newEntries.filter((entry) => entry.id !== id));
+  // };
+
+  return (
+    <>
+      <EntryForm addEntry={addEntry} />
       <Container className="m-auto">
-        <ListGroup.Item key={entry.date}>
-          {entry.date}
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Time</th>
-                <th>Product Name</th>
-                <th>Ingredients</th>
-                <th>Delete</th>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Product Name</th>
+              <th>Ingredients</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {entries.map((entry) => (
+              <tr key={entry.id}>
+                <th>{entry.date}</th>
+                <th>{entry.time}</th>
+                <th>{entry.name}</th>
+                <th>{entry.ingredients}</th>
+                <th>
+                  <CloseButton />
+                </th>
               </tr>
-            </thead>
-            <tbody>{listProducts}</tbody>
-          </Table>
-        </ListGroup.Item>
+            ))}
+          </tbody>
+        </Table>
       </Container>
-    );
-  });
-
-  return <ListGroup>{listEntries}</ListGroup>;
+    </>
+  );
 }
+//
--- frontend/src/components/EntryForm.js
+++ frontend/src/components/EntryForm.js
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/esm/Container";
 
-export default function EntryForm() {
+export default function EntryForm({ addEntry }) {
+  const [entry, setEntry] = useState({
+    id: uuidv4(),
+    date: "",
+    time: "",
+    name: "",
+    ingredients: "",
+  });
+
+  const handleAddEntry = (e) => {
+    e.preventDefault();
+    addEntry(entry);
+  };
+
   return (
     <Container className="m-auto">
       <Form className="m-3">
         <Form.Group className="mb-3" controlId="formBasicEntry">
+          <Form.Label>Date</Form.Label>
+          <Form.Control
+            className="mb-3"
+            type="text"
+            value={entry.date}
+            onChange={(e) => setEntry({ ...entry, date: e.target.value })}
+          />
           <Form.Label>Time</Form.Label>
-          <Form.Control className="mb-3" type="time" />
+          <Form.Control
+            className="mb-3"
+            type="text"
+            value={entry.time}
+            onChange={(e) => setEntry({ ...entry, time: e.target.value })}
+          />
           <Form.Label>Product Name</Form.Label>
-          <Form.Control className="mb-3" type="text" />
+          <Form.Control
+            className="mb-3"
+            type="text"
+            value={entry.name}
+            onChange={(e) => setEntry({ ...entry, name: e.target.value })}
+          />
           <Form.Label>Ingredients</Form.Label>
-          <Form.Control className="mb-3" as="textarea" />
+          <Form.Control
+            className="mb-3"
+            as="textarea"
+            value={entry.ingredients}
+            onChange={(e) =>
+              setEntry({ ...entry, ingredients: e.target.value })
+            }
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Add to entry
+        <Button variant="primary" type="submit" onClick={handleAddEntry}>
+          Add Entry
         </Button>
       </Form>
     </Container>
